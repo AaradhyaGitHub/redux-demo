865a32aacd4fa9ec3a96c86fa7675db40fa111ec
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-03-04 21:39:51 -0800

    Initialized initial state value, added reducer, subscriber and connected the subscriber to the store

--- redux-demo.js
+++ redux-demo.js
@@ -1,8 +1,8 @@
-const redux = require('redux');
+const redux = require("redux");
 
-//Creating Store, Reducer Function, Action and Component or in this case some sort of subscription 
+//Creating Store, Reducer Function, Action and Component or in this case some sort of subscription
 {
-    /*
+  /*
             Store is central but reducer is the one that communicates with it so we need to implement it 
             
             > Reducer func is a standard jS function. 
@@ -23,12 +23,26 @@ const redux = require('redux');
 
     */
 }
-//Reducer Function: 
-const counterReducer = (state, action ) => {
-    return {
-        counter: 0s
-    };
+//Reducer Function:
+//we want to give state a default starting value. Why?
+//When the store is initialized, this reducer function is executed but at this point, we have no existing state.
+const counterReducer = (state = { counter: 0 }, action) => {
+  return {
+    counter: state.counter + 1
+  };
 };
 
-//Store 
-const store = redux.createStore();
+//Store
+const store = redux.createStore(counterReducer);
+
+//Subscriber
+const counterSubscriber = () => {
+  //getState is available on the store and gives us the latest available state after it was updated
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+//making redux aware of the subscriber and it should be executed when the state is changed
+//subscribe expects a function -> That's why we pass counterSubscriber
+//do not execute funciton, just call it
+store.subscribe(counterSubscriber);
